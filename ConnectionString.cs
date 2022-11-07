35435beb467ea8c8b21c77a6b86609588a88e2db
Krysilen Cooks <krysilencooks@gmail.com>
2022-11-06 18:05:35 -0600

    created connection string class

---
+++ ConnectionString.cs
@@ -0,0 +1,11 @@
+namespace Api
+{
+    public class ConnectionString
+    {
+        public string cs {get;}
+
+        public ConnectionString(){
+            cs="server=http://h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;user=hllptjk6934naxti;database=fdfpqo2wtkd2rwyf;port=3306;password=g9sovn4chedx08w6";
+        }
+    }
+}
