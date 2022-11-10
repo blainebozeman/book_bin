4d32bd050324413f8cb1444adb25faa44ff725c8
Floop55555 <mtschmidt2@crimson.ua.edu>
2022-11-09 23:03:06 -0600

    basic log in

---
+++ Client/Resources/Scripts/emplogin.js
@@ -0,0 +1,52 @@
+let addForm = document.getElementById("login")
+
+function handleOnLoad()
+{
+    createForm();
+}
+
+function createForm()
+{
+    let form = document.createElement("form");
+
+    let user = document.createElement("input");
+    user.type = "text";
+    user.placeholder = "Username";
+    user.id = "username";
+    form.appendChild(user);
+
+    let pass = document.createElement("input");
+    pass.type = "text";
+    pass.placeholder = "Password";
+    pass.id = "password";
+    form.appendChild(pass);
+
+    let submitButton = document.createElement("button");
+    submitButton.textContent = "Submit";
+    submitButton.id = "Submit";
+    form.appendChild(submitButton);
+
+    form.addEventListener("submit", function(event)
+    {
+        event.preventDefault();
+        let user = 
+        {
+            username : event.target.elements.username.value,
+            rating : event.target.elements.password.value,
+            userType : "employee"
+        }
+
+        try
+        {
+            GetUser(user)
+        }
+        catch
+        {
+            document.createTextNode("Please enter a valid usernamae and password")
+
+        }
+    
+    })
+
+    addForm.appendChild(form);
+}
