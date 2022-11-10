98309b9a5d928f0d368a7239e5d7c766480371e7
Floop55555 <mtschmidt2@crimson.ua.edu>
2022-11-10 13:42:36 -0600

    added things

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.DataAccess;
+
+namespace book_bin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET: api/Employee
+        [HttpGet]
+        public List<Employees> Get()
+        {
+            EmployeeData dataAccess = new EmployeeData();
+            return dataAccess.GetAll();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Employee
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- API/Models/Employees.cs
+++ API/Models/Employees.cs
@@ -2,6 +2,11 @@ namespace API.Models
 {
     public class Employees
     {
-        
+        int Emp_ID {get;set;}
+        string EmpUserName {get;set;}
+        string EmpPassword {get;set;}
+        bool Admin {get;set;}
+        string FName {get;set;}
+        string LName {get;set;}
     }
 }
