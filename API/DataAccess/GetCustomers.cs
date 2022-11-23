3b1a563e21d048971e09efdc2eec2d9c8894fce9
Floop55555 <mtschmidt2@crimson.ua.edu>
2022-11-22 23:32:23 -0500

    cust login functional

--- API/DataAccess/GetCustomers.cs
+++ API/DataAccess/GetCustomers.cs
@@ -22,13 +22,13 @@ namespace API.DataAccess
                 System.Console.WriteLine("It's open");
             }
 
-            string stm = $"SELECT * from drivers WHERE CustUserName LIKE '{user.CustUserName}' and CustPassword LIKE '{user.CustPassword}';";
+            string stm = $"SELECT * from customers WHERE UserName LIKE '{user.CustUserName}' and CustPassword LIKE '{user.CustPassword}';";
             MySqlCommand cmd = new MySqlCommand(stm, con);
             MySqlDataReader rdr = cmd.ExecuteReader();
 
             while (rdr.Read())
             {
-                Customer newCustomer = new Customer() { CustID = rdr.GetGuid(0), FName = rdr.GetString(1), LName = rdr.GetString(2), Credits = rdr.GetInt32(3), CustUserName = rdr.GetString(4), Emp_ID = rdr.GetInt32(5)};
+                Customer newCustomer = new Customer() { CustID = rdr.GetInt32(0), FName = rdr.GetString(1), LName = rdr.GetString(2), Credits = rdr.GetInt32(3), CustUserName = rdr.GetString(4), Emp_ID = rdr.GetInt32(5), CustPassword = rdr.GetString(6)};
                 customers.Add(newCustomer);
             }
             con.Close();
