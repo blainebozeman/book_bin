496fa5fb93d81bf6aec9fea111bdabd87d86c7ea
Floop55555 <mtschmidt2@crimson.ua.edu>
2022-11-26 22:19:02 -0500

    add credits

---
+++ API/DataAccess/UpdateCustomers.cs
@@ -0,0 +1,40 @@
+using MySql.Data.MySqlClient;
+using API.Models;
+using API.Models.Interfaces;
+namespace API.DataAccess
+{
+    public class UpdateCustomers
+    {
+        public List<Customer> UpdateCredit(Customer user)
+        {
+            //test
+            System.Console.WriteLine(user.CustUserName);
+
+            //variables
+            List<Customer> customers = new List<Customer>();
+            ConnectionString connectionString = new ConnectionString();
+            string cs = connectionString.cs;
+            MySqlConnection con = new MySqlConnection(cs);
+
+            //Is it open?
+            try
+            {
+                System.Console.WriteLine("made it here");
+                con.Open();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("It's not open");
+            }
+            //update Credits
+            string stm = $"UPDATE `fdfpqo2wtkd2rwyf`.`customers` SET `CustCredits` = '{user.Credits}' WHERE (`cust_id` = '{user.CustID}');";
+            MySqlCommand cmd = new MySqlCommand(stm, con);
+            cmd.ExecuteNonQuery();
+            customers.Add(user);
+            con.Close();
+            System.Console.WriteLine(customers[0].CustUserName + customers[0].CustPassword);
+            con.Close();
+            return customers;
+        }
+    }
+}
