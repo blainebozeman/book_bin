e0fde959fb4eae592ab340b7205cf6a47d069de4
Floop55555 <mtschmidt2@crimson.ua.edu>
2022-11-27 17:13:37 -0500

    styling

--- Client/Resources/Scripts/CustLandingPage.js
+++ Client/Resources/Scripts/CustLandingPage.js
@@ -1,10 +1,21 @@
 let customer = JSON.parse(sessionStorage.getItem('customerUser'));
 let user = document.getElementById("user")
 
-function handleOnLoad()
-{
-    console.log(customer)
-}
+    function handleOnLoad()
+    {
+        console.log(customer)
+        CustomerID.textContent= "Customer ID: " + customer[0].custID;
+        CustomerName.textContent = "Customer Name: " + customer[0].fName + " " + customer[0].lName;
+        if(customer[0].credits != 0)
+        {
+            CustomerCredit.textContent = "Customer Credit: $" + customer[0].credits;
+        }
+        else 
+        {
+            customer[0].credits = 0
+            CustomerCredit.textContent = "Customer Credit: $" + customer[0].credits;
+        }
+    }
 function handleOnClick()
 {
     location.href = "CustSearchBooks.html"
