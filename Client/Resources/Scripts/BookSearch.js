d97e3ecbbefb8dcb2fd850dcb439442f55e81037
Krysilen Cooks <krysilencooks@gmail.com>
2022-11-28 00:11:34 -0600

    book search

--- Client/Resources/Scripts/BookSearch.js
+++ Client/Resources/Scripts/BookSearch.js
@@ -1,32 +1,30 @@
 const bookCardTemplate = document.querySelector("[data-book-template]")
 const bookCardContainer = document.querySelector("[data-book-cards-container]")
-const searchInput = document.querySelector("[data-search")
+const searchInput = document.querySelector("[data-search]")
 
 let books=[]
 
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
     books.forEach(book => {
-        const isVisble =
+        const isVisible =
         book.title.toLowerCase().includes(value)||
         book.author.toLowerCase().includes(value)
-        book.element.classList.toggle("hide", !isVisble)
+        book.element.classList.toggle("hide", !isVisible)
     })
 })
 
 fetch("https://localhost:5001/api/books")
 .then(res => res.json())
 .then(data => {
-    books = data.map(book=>{
-    //data.forEach(book => {
-    const bookCard = bookCardTemplate.content.cloneNode(true).children[0]
-    const header = bookCard.querySelector("[data-header]")
-    const body = bookCard.querySelector("[data-body]")
+    books = data.map(book => {
+     const card= bookCardTemplate.content.cloneNode(true).children[0]
+    const header = card.querySelector("[data-header]")
+    const body = card.querySelector("[data-body]")
     header.textContent = book.title
     body.textContent = book.author
     
-    bookCardContainer.append(bookCard)
-    
-    return {title: book.title, author: book.author, element: book}
+    bookCardContainer.append(card)
+    return {title: book.title, author: book.author, element: card}
 })
 })
