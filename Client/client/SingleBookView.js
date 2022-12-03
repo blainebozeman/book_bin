76563cc03eb5e4a2cfb1396e81fd3893b84012b4
Trey Ezell <treyezell@Treys-MacBook-Pro.local>
2022-12-02 21:29:47 -0600

    Updated Signlebookview.js - currently in progress

--- Client/client/SingleBookView.js
+++ Client/client/SingleBookView.js
@@ -3,38 +3,76 @@ function getBook(){
     var singleBookID = document.getElementById("singleBookID").value;
     // console.log(singleBookID);
     allBooksUrl += singleBookID;
-    allBooksUrl += '/';
+    // allBooksUrl += '/';
     const singleBookUrl = allBooksUrl;
-    console.log(singleBookUrl);
+    // console.log(singleBookUrl);
     document.getElementById("singlebookview").innerHTML="";
     
-    fetch(singleBookUrl).then(function(response){
-        console.log(response);
-        return response.json();
-    }).then(function(json){
-        console.log(json)
-        let ul = document.createElement("ul");
+    // fetch(singleBookUrl).then(function(response){
+    //     console.log(response);
+    //     return response.json();
+    // }).then(function(json){
+    //     console.log(json)
+
+
+    // //     let ul = document.createElement("ul");
         
-        let dateAdded = new Date(book.dateAdded);
-        let li = document.createElement("li");
+    // //     let dateAdded = new Date(book.dateAdded);
+    // //     let li = document.createElement("li");
 
-        // let BookCondition = 
-        // BookCondition.value=book.BookCondition;
+    // //     // let BookCondition = 
+    // //     // BookCondition.value=book.BookCondition;
 
-        li.innerHTML= `Book ID: ${book.bookID} <br />
-        Book Title: ${book.title} <br />
-        Date Added: ${dateAdded.toLocaleDateString('en-US')} <br />
-        Author: ${book.author} <br />
-        ISBN: ${book.isbn} <br />
-        Condition: ${book.condition} <br />`;
-        ul.appendChild(li);
-        ul.appendChild(li);
+    // //     li.innerHTML= `Book ID: ${book.bookID} <br />
+    // //     Book Title: ${book.title} <br />
+    // //     Date Added: ${dateAdded.toLocaleDateString('en-US')} <br />
+    // //     Author: ${book.author} <br />
+    // //     ISBN: ${book.isbn} <br />
+    // //     Condition: ${book.condition} <br />`;
+    // //     ul.appendChild(li);
+    // //     ul.appendChild(li);
        
-        document.getElementById("singlebookview").appendChild(ul);
-        console.log(json);
-    }).catch(function(error){
-        console.log(error);
-    });
+    // //     document.getElementById("singlebookview").appendChild(ul);
+    // //     console.log(json);
+    // // }).catch(function(error){
+    // //     console.log(error);
+    // });
+
+
+
+
+
+
+
+
+
+    // fetch(singleBookUrl).then(function(response){
+    //     console.log(response);
+    //     return response.json();
+    // }).then(function(json){
+    //     console.log(json)
+    //     let ul = document.createElement("ul");
+        
+    //     let dateAdded = new Date(book.dateAdded);
+    //     let li = document.createElement("li");
+
+    //     // let BookCondition = 
+    //     // BookCondition.value=book.BookCondition;
+
+    //     li.innerHTML= `Book ID: ${book.bookID} <br />
+    //     Book Title: ${book.title} <br />
+    //     Date Added: ${dateAdded.toLocaleDateString('en-US')} <br />
+    //     Author: ${book.author} <br />
+    //     ISBN: ${book.isbn} <br />
+    //     Condition: ${book.condition} <br />`;
+    //     ul.appendChild(li);
+    //     ul.appendChild(li);
+       
+    //     document.getElementById("singlebookview").appendChild(ul);
+    //     console.log(json);
+    // }).catch(function(error){
+    //     console.log(error);
+    // });
 }
 
 
